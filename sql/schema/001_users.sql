a87b2322da43a9b80098841984a5890ab5653bac
STaninnat <taninnat.s@gmailcom>
2024-10-15 03:23:26 +0700

    update user table

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -5,7 +5,7 @@ CREATE TABLE
         created_at TIMESTAMP NOT NULL,
         updated_at TIMESTAMP NOT NULL,
         name TEXT NOT NULL,
-        api_key TEXT UNIQUE NOT NULL
+        api_key VARCHAR(64) UNIQUE NOT NULL
     );
 
 -- +goose Down
